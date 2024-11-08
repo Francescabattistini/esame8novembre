eec9550c49dcec9ef72271cac8b17f27067d6e8a
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-11-08 12:16:23 +0100

    Initial commit

---
+++ src/main/java/francescabattistini/esame8novembre/Esame8novembreApplication.java
@@ -0,0 +1,13 @@
+package francescabattistini.esame8novembre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esame8novembreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esame8novembreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescabattistini/esame8novembre/Esame8novembreApplicationTests.java
@@ -0,0 +1,13 @@
+package francescabattistini.esame8novembre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esame8novembreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
